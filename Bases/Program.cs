38ba9b0c35379192e7ee24f5cf9193b7bedc5eb9
Alain Guillon <zyrass@outlook.fr>
2024-02-22 11:49:27 +0100

    Ajoutez des fichiers projet.

---
+++ Bases/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("BonjourJ'ai commencé le chapitre 2");
+Console.WriteLine("Tu sais comment je m'appel non ?");
+string? prenom = Console.ReadLine();
+Console.WriteLine($"C'était {prenom} en directe de mon ordinateur le {DateTime.Today}");
+
+
+Console.ReadKey(); // Permet d'attendre qu'un utilisateur clique sur une touche pour quitter le programme.
+
+
+// TABLEAU QUI DOIVENT ETRE DEFINIT OBLIGATOIREMENT SUR LE NOMBRE D'ELEMENT
+// Stockage des résultats d'un match de tennis
+byte?[] scoresJ1 = new byte?[5]; // Tableau Explicite qui ne peut pas être modifier
+byte?[] scoresJ2 = { 2, 6, 7, 3, 5 }; // Tableau Implicite qui ne peut pas être modifier
+
+scoresJ1[0] = 6;
+scoresJ1[1] = 5;
+scoresJ1[2] = 6;
+scoresJ1[3] = 7;
+scoresJ1[4] = 3;
+
+Console.WriteLine(scoresJ1.Length); // 5
+
---
+++ TypesEtVariables/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("==============================================================");
+Console.WriteLine(" TYPES ET VARIABLES");
+Console.WriteLine("==============================================================");
+Console.WriteLine("const définit une constante qui ne peut être modifiée");
+
+const string FIRSTNAME = "Alain";
+const string LASTNAME = "Guillon";
+
+Console.WriteLine(FIRSTNAME + " " + LASTNAME.ToUpper());
+
+Console.WriteLine("==============================================================");
+
+
+Console.WriteLine("STRING n'est pas une primitive");
+Console.WriteLine("==============================================================");
+Console.WriteLine("");
